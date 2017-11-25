19735a27b13efef873afbcf9e8e3b1a06dddd280
Maksymilian Skica <maks@riseup.net>
2017-11-25 12:35:04 +0100

    changed imail.py to notify.py, removed unimportand libs

--- investomat/notify.py
+++ investomat/notify.py
@@ -1,19 +1,18 @@
 """
-Module for email sending
+notyfing user
 """
 import smtplib
-from datetime import datetime
 from email.mime.text import MIMEText
 
 
 def send_email(subject, receipent, content, login, password, server, sender):
     """
-    sending emails
+    email sending
     """
-    mejl = 'From: %s\nSubject: %s\n%s' % (sender, subject, content)
+    mail = 'From: %s\nSubject: %s\n%s' % (sender, subject, content)
     s = smtplib.SMTP(host=server, port=587)
     s.starttls()
     s.login(login, password)
-    s.sendmail(sender, receipent, mejl)
+    s.sendmail(sender, receipent, mail)
     s.quit()
-    return ("sent!")
+    return True
