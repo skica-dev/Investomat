99faa23ffac4414ec0dcf446bdf3e24f69fdd643
m4k5 <maks@riseup.net>
2017-12-02 21:38:21 +0100

    unified quotemarks, expanded function to print balances

--- investomat/investomat.py
+++ investomat/investomat.py
@@ -20,16 +20,30 @@ try:
         api_secret = settings[1]
         address = settings[2]
 except (IOError, IndexError):
-    print "CONFIGURE.PY"
+    print 'CONFIGURE.PY'
     exit()
 
 
 exchange = bitcoin.BitBay_net(api_public, api_secret)
-bal = exchange.getBalance()
-for i in bal:
-    if bal[i]['available'] != "0" or bal[i]['locked'] != "0":
-        print "Available for {!s}: {!s}".format(i, bal[i]['available'])
-        print "Locked for {!s}: {!s}\n".format(i, bal[i]['locked'])
+exchange_balances = exchange.getBalance()
+exchange_price = exchange.btcPrice()
+result = ''
+for i in exchange_balances:
+    if (exchange_balances[i]['available'] != '0' or exchange_balances[i]['locked'] != '0'):
+        if i != 'PLN':
+            result += 'Available for {}: {} {}\n'.format(
+                i, exchange_balances[i]['available'], i)
+            result += 'Locked for {}: {} {}\n\n'.format(
+                i, exchange_balances[i]['locked'], i)
+        else:
+            result += 'Available for {}: {} {}\n'.format(
+                i, round(float(exchange_balances[i]['available']), 2), i)
+            result += 'Locked for {}: {} {}\n\n'.format(
+                i, round(float(exchange_balances[i]['locked']), 2), i)
+bitcoin_balance = bitcoin.getAddressBalance(address)
+result += 'Address balance is {!s} (~{!s} PLN)\n'.format(
+    bitcoin_balance, round(bitcoin_balance * exchange_price, 2))
+print result
 
 
 def count_wd(d0, d1, wd=4, f=0):
