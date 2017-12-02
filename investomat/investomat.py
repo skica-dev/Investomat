fe4b74cff55c4c0d35d9c059b388817619c800ad
m4k5 <maks@riseup.net>
2017-12-02 21:00:49 +0100

    code is now working, printing user's balances on exchange, solved \n bug

--- investomat/investomat.py
+++ investomat/investomat.py
@@ -12,14 +12,23 @@ except ImportError:
     print 'Missing dependencies, please check requirments.'
     exit()
 import notify
+import bitcoin
 try:
     with open('data') as data:
-        settings = data.readlines()
-        api_key = settings[0]
+        settings = data.read().splitlines()
+        api_public = settings[0]
         api_secret = settings[1]
         address = settings[2]
 except:
-    print "TODO"
+    print "CONFIGURE.PY"
+
+
+exchange = bitcoin.BitBay_net(api_public, api_secret)
+bal = exchange.getBalance()
+for i in bal:
+    if bal[i]['available'] != "0" or bal[i]['locked'] != "0":
+        print "Available for {!s}: {!s}".format(i, bal[i]['available'])
+        print "Locked for {!s}: {!s}\n".format(i, bal[i]['locked'])
 
 
 def count_wd(d0, d1, wd=4, f=0):
@@ -28,17 +37,4 @@ def count_wd(d0, d1, wd=4, f=0):
         if d0.weekday() == 0:
             f += 1
     return f
-
-
-'''
-bb_akt = float(info_bb()['PLN']['available']) + \
-    float(info_bb()['PLN']['locked'])
-btc_bb = float(info_bb()['BTC']['available']) + \
-    float(info_bb()['BTC']['locked'])
-btc = get_add_bal(address)
-actual = int((bb_akt + btc * btc_price()))
-print 'Aktualna wartość oszczędności:       ' + '\033[1m' + '\033[92m' + str(actual) + ' PLN' + '\033[0m'
-przew = actual + kw_pia * count_wd(datetime.date.today(), data_doc)
-print 'Przewidywana wartość oszczędności:   ' + '\033[1m' + str(int(przew * (1.0 - float(info_bb(False)['fee']) / 100))) + ' PLN' + '\033[0m'
-'''
-print imejl.send_email('Kasomat %s' % datetime.date(), 'maks1823@protonmail.com', 'akt')
+# print imejl.send_email('Kasomat %s' % datetime.date(), 'maks1823@protonmail.com', 'akt')
