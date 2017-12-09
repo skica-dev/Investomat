55922d64a79ccbbfaf9f99865ed8ed949aaeae88
m4k5 <maks@riseup.net>
2017-12-09 22:35:57 +0100

    more customisation available

--- investomat/notify.py
+++ investomat/notify.py
@@ -11,13 +11,13 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def send_email(subject, receipent, content, login, password, server):
+def send_email(subject, receipent, content, login, password, server, port=587):
     """
     email sending
     """
     try:
         mail = 'From: %s\nSubject: %s\n%s' % (login, subject, content)
-        s = smtplib.SMTP(host=server, port=587)
+        s = smtplib.SMTP(host=server, port=port)
         s.starttls()
         s.login(login, password)
         s.sendmail(login, receipent, mail)
