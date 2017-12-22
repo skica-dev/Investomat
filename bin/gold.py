8fe5136f43f3b7421004857b45528c602b03c948
m4k5 <maks@riseup.net>
2017-12-22 17:13:29 +0100

    removed re as they are unnecessary here

--- bin/gold.py
+++ bin/gold.py
@@ -1,12 +1,10 @@
-import re
-
 import bs4
 import requests
 
 
 def gold_price(keyword):
     """
-    fetches actual market's gold selling prives from zlotagotowka.pl, searches and return requested one/
+    fetches actual market's gold selling prives from zlotagotowka.pl, searches and return requested one
     """
     strona = requests.get('https://www.zlotagotowka.pl')
     strona.raise_for_status()
@@ -14,5 +12,5 @@ def gold_price(keyword):
     prices = html.select('tr')
     for i in range((len(prices))):
         position = prices[i].getText()
-        if re.search(keyword, position):
+        if keyword in position:
             return float(position[position[1:].find("\n") + 1:])
